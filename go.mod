d5c9d37d75241cc60e7bebc3b83afb6848d51b8f
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2022-10-18 00:31:29 -0300

    bumpo v

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/leolas95/mymodule/v2 v2.0.0
+module github.com/leolas95/mymodule/v3 v3.0.0
 
 go 1.18
--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,3 +3,8 @@ package greetings
 func Greet(name string) string {
     return fmt.Sprintf("hello %s", name)
 }
+
+func Bye() string {
+    return "bye"
+}
+
